5ce907225c52f83285647979f4bdd088e5b797f5
mwvabes <wielgos.me@gmail.com>
2020-05-28 09:26:06 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export default class Footer extends React.Component {
     return (
       <>
         <footer>
-          <p>&copy; 2018-2020 by <a href="http://vabes.pl">Marcin Wielgos, vabes.pl</a></p>
+          <p>&copy; 2018-2020 by <a href="https://wielgos.me">Marcin Wielgos, https://wielgos.me</a></p>
           <p>Wszelkie prawa zastrzeżone. Kopiowanie bez pozwolenia zabronione.</p>
         </footer>
       </>
