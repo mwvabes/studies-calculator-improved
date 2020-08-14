ef07e6784b698481a66b4a42f2d3853f96f0a68f
mwvabes <wielgos.me@gmail.com>
2020-08-14 21:56:43 +0200

    Update styles, meta & info

--- src/components/Warning.js
+++ src/components/Warning.js
@@ -10,10 +10,9 @@ export default class Warning extends React.Component {
       <>
 
         <div className="BigWarning">
-          <h1>Uwaga!</h1>
-          <p>Zbliża się okres letniej rekrutacji 2020. Zostanie w niej wprowadzony nowy sposób liczenia punktów (opisany na stronie PRz).</p>
-          <p>Niniejszy Kalkulator działa poprawnie względem poprzednich zasad rekrutacji, ale nie oblicza punktów dla nowego sposobu. Dlatego więc w systemie rekrutacyjnym zobaczycie inny wynik niż ten prezentowany przez Kalkulator.</p>
-          <p>Kalkulator ma na celu porównanie wyników z progami z poprzednich lat.</p>
+          <h1>Drodzy Kandydaci!</h1>
+          <p>Kalkulator oblicza wyniki porównawcze dla lat 2017 - 2019. <strong>Nie oblicza punktów dla rekrutacji 2020.</strong></p>
+          <p>Kalkulator ma na celu porównanie wyników względem poprzednich lat. Nie prezentuje on punktów dla rekturacji 2020 (punkty w Systemie Rekrutacyjnym będą inne niż w niniejszym kalkulatorze).</p>
         </div>
 
       </>
