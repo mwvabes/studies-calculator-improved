9b52909cab4bcfca071f95bd123c22c06f63f41c
mwvabes <wielgos.me@gmail.com>
2020-08-20 21:38:26 +0200

    Update ReportIssue.js

--- src/components/ReportIssue.js
+++ src/components/ReportIssue.js
@@ -9,7 +9,7 @@ export default class ReportIssue extends React.Component {
         <div className="ReportIssue">
           <div className="ReportIssueWrapper">
             <p>Znalazłeś błąd? Masz pytanie?</p>
-            <p>Śmiało, napisz na maila <strong>kontakt.vabes@gmail.com</strong></p>
+            <p>Daj mi o tym znać na maila <strong>kontakt.vabes@gmail.com</strong></p>
             <p>Jednocześnie dziękuję wszystkim,</p>
             <p>którzy przyczynili się do powstania niniejszego Kalkulatora, zgłosili błędy i podzielili się swoimi uwagami.</p>
           </div>
